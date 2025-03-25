c3115a4185919db5e06dbe3b87e608176d8c010e
“rheeghang <ryujh21h@pati.kr>
2025-03-26 02:16:03 +0900

    Improve component structure and accessibility

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './Pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
 import ExhibitionText from './components/ExhibitionText'
+import Menu from './components/Menu'
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<ExhibitionText />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <div className="App">
+        <Menu />
+        <Routes>
+          <Route path="/" element={<ExhibitionText />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
     </Router>
   )
 }
