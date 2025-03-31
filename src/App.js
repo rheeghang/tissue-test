bc01901a918be62062819d4c2457e85b994b3cb9
“rheeghang <ryujh21h@pati.kr>
2025-03-31 10:40:17 +0900

    스크롤바 숨김 처리 및 Page3 컨테이너 스타일 수정

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Intro from './Pages/Intro'
 import Page1 from './Pages/Page1'
 import Home2 from './Pages/Home2'
 import Page2 from './Pages/Page2'
+import Page3 from './Pages/Page3'
 import { AngleModeProvider } from './contexts/AngleModeContext'
 import { GuideProvider } from './contexts/GuideContext'
 
@@ -18,6 +19,7 @@ function App() {
               <Route path="/" element={<Home2 />} />
               <Route path="/1" element={<Page1 />} />
               <Route path="/2" element={<Page2 />} />
+              <Route path="/3" element={<Page3 />} />
               <Route path="/intro" element={<Intro />} />
             </Routes>
           </Router>
